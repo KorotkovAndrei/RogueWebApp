77d92061a840743a6e393cd91ddefe93048be2e6
Andrei Korotkov <example@gmail.com>
2020-06-01 04:01:18 +0200

    port 5432 again

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,11 +2,9 @@ const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
+console.log(process.env.PORT);
 module.exports = merge(common, {
-app.listen(5432, function () {
-  console.log('Example app listening on port 5432!');
-}),
+
     mode: 'production',
     plugins: [
         new CleanWebpackPlugin(),
