a76af3746c926af178033e52aa0eabf0465b59cb
Andrei Korotkov <example@gmail.com>
2020-06-01 02:22:43 +0200

    new runner

--- src/main/java/rogue/rogueWebApp/App.java
+++ src/main/java/rogue/rogueWebApp/App.java
@@ -1,13 +1,17 @@
 package rogue.rogueWebApp;
 
-import io.sentry.Sentry;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class App {
-	public static void main(String[] args) {
-		Sentry.capture("Application started");
-		SpringApplication.run(App.class, args);
+
+
+	@SpringBootApplication
+	public class App {
+
+		public static void main(String[] args) {
+			SpringApplication.run(App.class, args);
+		}
+
 	}
-}
+
