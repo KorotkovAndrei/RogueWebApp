1898f4549a62d30dd7b8940e8be2a1b771bd04f3
Andrei Korotkov <example@gmail.com>
2020-06-02 10:22:04 +0200

    tryin set port8

--- src/main/resources/js/main.js
+++ src/main/resources/js/main.js
@@ -2,11 +2,7 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import App from 'pages/App.vue'
-const port = argv.port || 8081;
 
-app.listen(argv.port, ()=>{
-    console.log('Probably listening to heroku $PORT now ', argv.port); // unless $PORT is undefined, in which case you're listening to 8081.
-});
 Vue.use(VueResource)
 new Vue({
 el: '#app',
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,8 @@ const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
+var server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
+var server_host = process.env.YOUR_HOST || ‘0.0.0.0’;
 module.exports = merge(common, {
     mode: 'production',
     plugins: [
@@ -13,4 +14,13 @@ module.exports = merge(common, {
         filename: 'main.js',
         path: path.resolve(__dirname, 'src', 'main', 'resources','static', 'js'),
     },
+    devServer: {
+     disableHostCheck: true,
+     contentBase: ‘./dist’,
+     compress: true,
+     inline: true,
+     port:server_port,
+     host:server_host
+
+    }
 });
