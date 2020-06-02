454258c679de33d0e113656217cbc1fdd32a0256
Andrei Korotkov <example@gmail.com>
2020-06-02 09:30:27 +0200

    tryin set port4

--- src/main/resources/js/main.js
+++ src/main/resources/js/main.js
@@ -2,11 +2,11 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import App from 'pages/App.vue'
-
+const PORT = process.env.PORT || 5000
 Vue.use(VueResource)
 new Vue({
 el: '#app',
-render: a => a(App)
+render: a => a(App).listen(PORT, () => console.log(`Listening on ${ PORT }`))
 })
 
 
