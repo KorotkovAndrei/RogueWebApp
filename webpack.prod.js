c0777c5b7c657892f800b891ac8ab5d8c8f36b99
Andrei Korotkov <example@gmail.com>
2020-06-02 09:25:40 +0200

    tryin set port3

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,9 +3,6 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = merge(common, {
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
     mode: 'production',
     plugins: [
         new CleanWebpackPlugin(),
