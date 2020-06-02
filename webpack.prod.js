565b91a7a51a5d8a41cca98b398f851b1e014bd0
Andrei Korotkov <example@gmail.com>
2020-06-02 10:40:47 +0200

    tryin set port10

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,8 +2,7 @@ const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-var server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
-var server_host = process.env.YOUR_HOST || ‘0.0.0.0’;
+
 module.exports = merge(common, {
     mode: 'production',
     plugins: [
@@ -14,13 +13,4 @@ module.exports = merge(common, {
         filename: 'main.js',
         path: path.resolve(__dirname, 'src', 'main', 'resources','static', 'js'),
     },
-    devServer: {
-
-     contentBase: ‘./dist’,
-     compress: true,
-
-     port:server_port,
-     host:server_host
-
-    }
 });
