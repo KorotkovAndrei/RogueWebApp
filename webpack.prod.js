acb53b3e0e4080691dac1e8fcaedb6cc52b564af
Andrei Korotkov <example@gmail.com>
2020-06-02 10:05:56 +0200

    tryin set port7

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,10 +2,7 @@ const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-var port = process.env.PORT || 3000;
-app.listen(port, "0.0.0.0", function() {
-console.log("Listening on Port 3000");
-});
+
 module.exports = merge(common, {
     mode: 'production',
     plugins: [
