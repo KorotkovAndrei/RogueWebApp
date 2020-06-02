595e00d3b87e68cf06fa3667697586feddda9edc
Andrei Korotkov <example@gmail.com>
2020-06-02 09:22:41 +0200

    tryin set port2

--- src/main/resources/js/main.js
+++ src/main/resources/js/main.js
@@ -2,9 +2,7 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import App from 'pages/App.vue'
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+
 Vue.use(VueResource)
 new Vue({
 el: '#app',
